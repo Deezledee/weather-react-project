cf5fbf733a9ed25ab0d716dc18ea1fbb3a05e181
Diana <dianakalstein@gmail.com>
2022-02-26 14:09:05 +0100

    Added a container

--- src/Weather.js
+++ src/Weather.js
@@ -13,112 +13,114 @@ export default function Weather() {
 
   return (
     <div className="Weather">
-      <h1>🌡️ Weather Forecast 🌡️</h1>
+      <div className="container">
+        <h1>🌡️ Weather Forecast 🌡️</h1>
 
-      <br />
-      <form>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter a city name"
-          autoComplete="on"
-        />
-        <input type="submit" value="Search 🔍" className="form-control" />{" "}
         <br />
-        <button className="btn btn-success">
-          Current location <span>📍</span>
-        </button>
-      </form>
-      <br />
-
-      <h2>Today in Berlin</h2>
-      <br />
-      <p>
-        <span className="today-temperature">19</span>
-        <span className="units">
-          <a href="/">°C</a> |<a href="/">°F</a>
-        </span>
+        <form>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter a city name"
+            autoComplete="on"
+          />
+          <input type="submit" value="Search 🔍" className="form-control" />{" "}
+          <br />
+          <button className="btn btn-success">
+            Current location <span>📍</span>
+          </button>
+        </form>
         <br />
-      </p>
 
-      <div className="todayDescription">
-        <span>{weatherData.description}</span>
-        <img
-          src="http://openweathermap.org/img/wn/10d@2x.png"
-          alt="clear sky"
-          className="float-right"
-          width="50px"
-        />
-      </div>
-      <br />
-      <div className="row paremeters">
-        <div className="col humidity">
-          💧 Humidity: <span>{weatherData.humidity}</span>%
+        <h2>Today in Berlin</h2>
+        <br />
+        <p>
+          <span className="today-temperature">19</span>
+          <span className="units">
+            <a href="/">°C</a> |<a href="/">°F</a>
+          </span>
+          <br />
+        </p>
+
+        <div className="todayDescription">
+          <span>{weatherData.description}</span>
+          <img
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt="clear sky"
+            className="float-right"
+            width="50px"
+          />
         </div>
-        <div className="col windspeed">
-          💨 Windspeed:
-          <span>{weatherData.windspeed}</span> m/s
+        <br />
+        <div className="row paremeters">
+          <div className="col humidity">
+            💧 Humidity: <span>{weatherData.humidity}</span>%
+          </div>
+          <div className="col windspeed">
+            💨 Windspeed:
+            <span>{weatherData.windspeed}</span> m/s
+          </div>
         </div>
-      </div>
-      <br />
-      <h3 className="date">{weatherData.date}</h3>
-      <br />
+        <br />
+        <h3 className="date">{weatherData.date}</h3>
+        <br />
 
-      <div className="weather-forecast">
-        <div className="row">
-          <div className="col-2">
-            <div className="weather-forecast-date">Thu</div>
-            <img
-              src="http://openweathermap.org/img/wn/50d@2x.png"
-              alt=""
-              width="40"
-            />
-            <div className="weather-forecast-temperatures">
-              <span className="weather-forecast-temperature-max">
-                {" "}
-                {weatherData.temperature}°{" "}
-              </span>
-              <span className="weather-forecast-temperature-min">
-                {" "}
-                {weatherData.temperature}°{" "}
-              </span>
-              <br />
-              <br />
+        <div className="weather-forecast">
+          <div className="row">
+            <div className="col-2">
+              <div className="weather-forecast-date">Thu</div>
+              <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="40"
+              />
+              <div className="weather-forecast-temperatures">
+                <span className="weather-forecast-temperature-max">
+                  {" "}
+                  {weatherData.temperature}°{" "}
+                </span>
+                <span className="weather-forecast-temperature-min">
+                  {" "}
+                  {weatherData.temperature}°{" "}
+                </span>
+                <br />
+                <br />
 
-              <footer class="codedby">
-                Coded by{" "}
-                <span>
-                  <a
-                    href="https://www.shecodes.io/students/503-diana-kalstein"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {" "}
-                    Diana Kalstein ©️
-                  </a>
-                </span>
-                , is{" "}
-                <span>
-                  <a
-                    href="https://github.com/Deezledee/Weather-App-Diana-K.git"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {" "}
-                    open-sourced on GitHub{" "}
-                  </a>
-                </span>
-                and is{" "}
-                <span>
-                  <a
-                    href="https://adoring-kowalevski-9714eb.netlify.app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    hosted on Netlify
-                  </a>
-                </span>
-              </footer>
+                <footer class="codedby">
+                  Coded by{" "}
+                  <span>
+                    <a
+                      href="https://www.shecodes.io/students/503-diana-kalstein"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {" "}
+                      Diana Kalstein ©️
+                    </a>
+                  </span>
+                  , is{" "}
+                  <span>
+                    <a
+                      href="https://github.com/Deezledee/Weather-App-Diana-K.git"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {" "}
+                      open-sourced on GitHub{" "}
+                    </a>
+                  </span>
+                  and is{" "}
+                  <span>
+                    <a
+                      href="https://adoring-kowalevski-9714eb.netlify.app"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      hosted on Netlify
+                    </a>
+                  </span>
+                </footer>
+              </div>
             </div>
           </div>
         </div>
