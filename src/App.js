7e49630d1df193aa8410d13dcc2134493d424111
Diana <dianakalstein@gmail.com>
2022-02-28 11:32:44 +0100

    Added image

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <img src={require("./images/sunflower2.jpg").default} />
     </div>
   );
 }
+
+export default App;
