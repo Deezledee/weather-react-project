687105b698e1e528e92917b651a4895360c609f7
Diana <dianakalstein@gmail.com>
2022-02-28 11:22:03 +0100

    Create Sunflower2.js

---
+++ src/Sunflower2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import sunflower2 from "./sunflower2.jpg";
+
+function Sunflower2() {
+  console.log(sunflower2);
+
+  return (
+    <div className="App">
+      <img src={sunflower2} alt="sunflower2 image" />
+    </div>
+  );
+}
+
+export default Sunflower2;
